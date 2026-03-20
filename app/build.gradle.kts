9fdef31460ee481556d065d76e3537eee8fff804
Naman Agrawal <nam.agr96@gmail.com>
2026-03-20 21:32:41 +0530

    Updated targetSdk for updated version 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.runanywhere.kotlin_starter_example"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.runanywhere.kotlin_starter_example"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
